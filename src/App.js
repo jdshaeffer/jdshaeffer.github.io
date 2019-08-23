2648751419725935d7462f7356dc8d63cbc96458
jdshaeffer <shaefferjd@gmail.com>
2019-08-23 14:15:45 -0700

    gets rid of background images - troubles with css

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import './App.css';
-import background from './assets/casey-horner-O0R5XZfKUGQ-unsplash.jpg'
+import background from './assets/casey-horner-O0R5XZfKUGQ-unsplash.jpg' // mountain picture
+import tiles from './assets/tile.png' // pretty disorienting black and white tiles
 
 function App() {
   return (
-    <div className='App' style={{backgroundImage: "url(" + background + ")"}}> 
-
-    </div >
+    // <div className='App' style={{backgroundImage: "url(" + tiles + ")"}}>
+    <div className='App'>
+      <h1>j.d. shaeffer</h1>
+    </div>
   );
 }
 
