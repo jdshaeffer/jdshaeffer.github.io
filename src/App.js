46d78dfa7dc94b01056c59db4e1036c3bd42a6a6
jdshaeffer <shaefferjd@gmail.com>
2019-08-23 05:16:36 -0700

    sets background picture at full screen

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
+import background from './assets/casey-horner-O0R5XZfKUGQ-unsplash.jpg'
 
 function App() {
   return (
-    <div className="App">
-    </div>
+    <div className='App' style={{backgroundImage: "url(" + background + ")"}}> 
+
+    </div >
   );
 }
 
