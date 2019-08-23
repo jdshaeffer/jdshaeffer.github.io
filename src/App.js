fbe8816b9509b9e3d713db8717219237a11725b0
jdshaeffer <shaefferjd@gmail.com>
2019-08-23 15:53:04 -0700

    fixes resume.pdf

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './App.css';
+import resume from './assets/resume.pdf'
 
 document.body.className = 'App'
 
@@ -9,7 +10,7 @@ function App() {
     <div>
       <h1>j.d. shaeffer</h1>
       <div className="Links">
-        <a href='assets/resume.pdf'><h1>resume</h1></a>
+        <a href={ resume }><h1>resume</h1></a>
         <a href='https://github.com/jdshaeffer'><h1>github</h1></a>
         <a href='https://www.linkedin.com/in/j-d-s-0b5751132/'><h1>linkedin</h1></a>
       </div>
