4f5dd9a7a04a8c67e10bb2e7b2dd4f41b6139a1e
jdshaeffer <shaefferjd@gmail.com>
2019-10-26 19:35:33 -0700

    starting react router - will come back

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import { browserHistory, Router, Route } from 'react-router';
+
 import './App.css';
 import resume from './assets/resume.pdf'
 
@@ -7,15 +9,18 @@ document.body.className = 'App'
 function App() {
   return (
     // <div className='App' style={{backgroundImage: "url(" + tiles + ")"}}>
-    <div>
-      <h1>j.d. shaeffer</h1>
-      <div className="Links">
-        <a href={ resume }><h1>resume</h1></a>
-        <a href='https://github.com/jdshaeffer'><h1>github</h1></a>
-        <a href='https://www.linkedin.com/in/j-d-s-0b5751132/'><h1>linkedin</h1></a>
-        <a href='tic.html'><h1>hmm</h1></a> 
+    <Router>
+      <div>
+        <h1>j.d. shaeffer</h1>
+        <div className="Links">
+          <a href={ resume }><h1>resume</h1></a>
+          <a href='https://github.com/jdshaeffer'><h1>github</h1></a>
+          <a href='https://www.linkedin.com/in/j-d-s-0b5751132/'><h1>linkedin</h1></a>
+          {/* <a href='tic.html'><h1>hmm</h1></a>  */}
+          <Route path='tic.html' render={() => {window.location.href='tic.html'}} />
+        </div>
       </div>
-    </div>
+    </Router>
   );
 }
 
