ce9384a9ded7cdd3621ee8d15cf1ef96e25b4be9
jdshaeffer <shaefferjd@gmail.com>
2019-10-26 19:16:21 -0700

    starting new experiment

--- src/App.js
+++ src/App.js
@@ -13,6 +13,7 @@ function App() {
         <a href={ resume }><h1>resume</h1></a>
         <a href='https://github.com/jdshaeffer'><h1>github</h1></a>
         <a href='https://www.linkedin.com/in/j-d-s-0b5751132/'><h1>linkedin</h1></a>
+        <a href='tic.html'><h1>hmm</h1></a> 
       </div>
     </div>
   );
---
+++ src/Tic.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+function Tic() {
+    return(
+        <h1>hello?</h1>
+    );
+}
+
+export default Tic;
