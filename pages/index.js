2e7058ac0e799865bbb0fe92c66d3fa6a1c2d276
jdshaeffer <shaefferjd@gmail.com>
2020-02-07 13:06:01 -0700

    moving project to next

---
+++ pages/index.js
@@ -0,0 +1,14 @@
+import Head from 'next/head';
+
+const index = () => {
+	return (
+		<>
+			<Head>
+				<title>yahhh</title>
+			</Head>
+			<h1>sup dood</h1>
+		</>
+	)
+}
+
+export default index;
--- src/Tic.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-function Tic() {
-    return(
-        <h1>hello?</h1>
-    );
-}
-
-export default Tic;
