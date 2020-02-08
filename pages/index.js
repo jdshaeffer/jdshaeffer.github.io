f53e172b0b72d2130c6cca4288e193ef0c4e56f8
jdshaeffer <shaefferjd@gmail.com>
2020-02-08 13:56:51 -0700

    component reorganization

--- pages/index.js
+++ pages/index.js
@@ -1,25 +1,15 @@
 import Head from 'next/head';
-import Link from 'next/link';
-import { useState } from 'react';
 import { createGlobalStyle, ThemeProvider } from "styled-components";
-import { reset, themes, Window, WindowHeader, WindowContent, Toolbar, Button, Bar, Hourglass } from "react95";
+import { reset, themes } from "react95";
 
-import { ContactButton } from '../components/ContactButton';
 import { Images } from '../components/Images';
+import { CustomWindow } from '../components/CustomWindow';
 
 const ResetStyles = createGlobalStyle`
   ${reset}
 `;
 
 const index = () => {
-	const [content, setContent] = useState(
-		<>
-			<p>oh hey.</p>
-			<p>i'm j.d.</p>
-			<p>glad you're here.</p>
-			<p>stay as long as you like.</p>
-		</>
-	);
 	return (
 		<>
 			<Head>
@@ -32,51 +22,7 @@ const index = () => {
 					<p>gotcha haha</p>
 				</div>
 				<div style={{position: 'absolute', left: '50%', top: '25%', transform: 'translate(-50%, 50%)'}}>
-					<Window style={{width: 500}}>
-						<WindowHeader style={{display: 'flex', alignItems: 'center', justifyContent: 'space-between'}}>
-							<span>j.d.'s website</span>
-							<Button style={{ marginRight: '-6px', marginTop: '1px' }} size={'sm'} square onClick={(event) => {
-								// close the window
-								event.target.parentElement.parentElement.parentElement.style = 'display: none';
-							}}>
-								<span style={{ fontWeight: 'bold', transform: 'translateY(-1px)' }}>x</span>
-							</Button>
-						</WindowHeader>
-						<Toolbar>
-							<Bar />
-							<Button variant='menu' size='sm' onClick={() => {
-								setContent(
-									<>
-										<p>the gear calculator (in development, stay tuned)</p>
-										<p>~~~~ </p>
-										<Link href='https://github.com/jdshaeffer/dunnet-solver'>
-											<a>dunnet solver (fun python automation for dunnet by ron schnell)</a>
-										</Link>
-										<p>~~~~ </p>
-									</>
-								)
-							}}>
-								projects
-							</Button>
-							<Button variant='menu' size='sm' onClick={() => {
-								setContent(
-									<>
-										<p>i do full stack web dev in the form of sick javascript apps.</p>
-										<Link href='/resume.pdf'>
-											<a><Hourglass /></a>
-										</Link>
-										<p>^^^ click to peek my resume</p>
-									</>
-								)
-							}}>
-								about
-							</Button>
-							<ContactButton />
-						</Toolbar>
-						<WindowContent>
-							{content}
-						</WindowContent>
-					</Window>
+					<CustomWindow />
 				</div>
 			</ThemeProvider>
 		</>
