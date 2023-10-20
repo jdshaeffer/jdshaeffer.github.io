030b7b44353b32fa5379c824aadad52943a059d3
JD Shaeffer <shaefferjd@gmail.com>
2023-10-20 13:04:07 -0700

    content and styles

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,35 @@ import './App.css';
 function App() {
   return (
     <div className='app'>
-      <div className='app-header'>
+      <div className='column'>
         <h1>hey</h1>
+        <h2>i'm jd</h2>
+        <p>welcome to this little site on the web</p>
+        <p>hosting some projects here:</p>
+        <ul className='project-list'>
+          <li>
+            <a href='https://jdshaeffer.github.io/bikepacking-events/'>
+              bikepacking-events
+            </a>
+          </li>
+          <li>
+            <a href='https://jdshaeffer.github.io/dungeon-solitaire/'>
+              dungeon-solitaire
+            </a>
+          </li>
+          <li>
+            <a href='https://jdshaeffer.github.io/hips/'>hips</a>
+          </li>
+          <li>
+            <a href='https://jdshaeffer.github.io/nycmud/'>nycmud</a>
+          </li>
+          <li>
+            <a href='https://jdshaeffer.github.io/journal/'>journal</a>
+          </li>
+        </ul>
+      </div>
+      <div className='column'>
+        <small>questions/feedback? gmail at shaefferjd</small>
       </div>
     </div>
   );
